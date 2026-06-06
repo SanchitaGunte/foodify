ff9d0d73b47b03734f48eb4d528edd317a2e350f
Sanchita <sanchitagunte11@gmail.com>
2026-06-06 09:38:48 +0530

    Day - 05 (folder format 02)

--- src/components/body.js
+++ src/components/body.js
@@ -1,10 +1,31 @@
 import RestaurantCard from "./restaurantcard";
 import { restlist } from "../utils/constants";
+import { useState } from "react"; 
 const Body = () => {
+    const [listOfRestaurants, setlistofrestaurant] = useState(restlist);
   return (
     <div className="body">
       <div className="search">search</div>
-      <div className="res-container"></div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4,
+            );
+            setlistofrestaurant(filteredList);
+          }}
+        >
+            Top Rated Restaurants 
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => {
+            return(
+                <RestaurantCard key ={restaurant.info.id} restData={restaurant} />
+            );
+        })}
+      </div>
       {restlist.map((restaurant) => {
         return (
           <RestaurantCard key={restaurant.info.id} restData={restaurant} />
